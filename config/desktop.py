6083ad692036d96e9f8fcab0ecfc9809483f93d4
AslanMuskSol <aslanmusk.sol@gmail.com>
2025-08-17 18:22:50 +0530

    Update desktop.py

--- config/desktop.py
+++ config/desktop.py
@@ -1,1 +1,13 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Pharma Billing",
+            "color": "blue",
+            "icon": "octicon octicon-file",
+            "type": "module",
+            "label": _("Pharma Billing")
+        }
+    ]
 
