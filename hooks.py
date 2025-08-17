da936a7b84985da0eb6d0144c9c47fc3fa8bde06
AslanMuskSol <aslanmusk.sol@gmail.com>
2025-08-17 18:29:43 +0530

    Update hooks.py

--- hooks.py
+++ hooks.py
@@ -1,8 +1,20 @@
+from . import __version__ as app_version
+
 app_name = "pharma_billing"
 app_title = "Pharma Billing"
 app_publisher = "Your Name"
-app_description = "Custom app for Pharmacy Billing on ERPNext"
+app_description = "Custom Pharma Billing App for ERPNext"
+app_icon = "octicon octicon-file"
+app_color = "blue"
 app_email = "you@example.com"
 app_license = "MIT"
 
-# Fixtures, custom scripts, and dashboard configs will be added later
+# Includes in <head>
+app_include_css = []
+app_include_js = []
+
+# Home Page
+home_page = "workspace"
+
+# Fixtures (we will use later for WhatsApp Templates etc.)
+fixtures = []
