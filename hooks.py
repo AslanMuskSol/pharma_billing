ad82b858e3e6441451742bab9c654c2eb055ec61
AslanMuskSol <aslanmusk.sol@gmail.com>
2025-08-17 18:08:21 +0530

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,8 @@
+app_name = "pharma_billing"
+app_title = "Pharma Billing"
+app_publisher = "Your Name"
+app_description = "Custom app for Pharmacy Billing on ERPNext"
+app_email = "you@example.com"
+app_license = "MIT"
+
+# Fixtures, custom scripts, and dashboard configs will be added later
